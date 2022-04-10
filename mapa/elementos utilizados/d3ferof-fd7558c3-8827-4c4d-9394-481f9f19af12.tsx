a7f8f256c5200d7f0c7614443f8272748e9a0661
cjcre <cjcrespo5@gmail.com>
2022-04-10 17:56:25 +0200

    mapa

---
+++ mapa/elementos utilizados/d3ferof-fd7558c3-8827-4c4d-9394-481f9f19af12.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="d3ferof-fd7558c3-8827-4c4d-9394-481f9f19af12" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="d3ferof-fd7558c3-8827-4c4d-9394-481f9f19af12.png" width="384" height="192"/>
+</tileset>
