4230ea2e750391828f77ef0d214c3310a8689e27
cjcre <cjcrespo5@gmail.com>
2022-04-18 17:53:53 +0200

    mapa2 & sprites

---
+++ mapa2/archivos mapa/mr02b08t73 (3).tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="mr02b08t73 (3)" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="../png/mr02b08t73 (3).png" width="128" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0415342" y="0" width="32.0228" height="32.189"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0830683" width="32.1059" height="31.8982"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.124603" y="0.124603" width="31.9398" height="31.8152"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0830683" y="0" width="32.1474" height="32.0228"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.0415342" width="32.0644" height="31.8982"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.166137" width="31.9813" height="25.7927"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0415342" y="-0.124603" width="31.9398" height="26.0419"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0415342" y="0.0830683" width="31.8982" height="31.9813"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ mapa2/archivos mapa/mr02b08t73.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="mr02b08t73" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="mr02b08t73.png" width="64" height="32"/>
+</tileset>
