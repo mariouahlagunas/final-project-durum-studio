a8fe15c667db2ff007df609c1c31a2807fef65fb
cjcre <cjcrespo5@gmail.com>
2022-04-28 20:41:57 +0200

    prueba mapa para nico

--- pruebaMapa/caja.tsx
+++ pruebaMapa/caja.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="caja" tilewidth="300" tileheight="300" tilecount="1" columns="1">
- <image source="caja.png" width="400" height="428"/>
+<tileset version="1.8" tiledversion="1.8.4" name="caja" tilewidth="7" tileheight="7" tilecount="1764" columns="42">
+ <image source="caja.png" width="300" height="300"/>
 </tileset>
--- pruebaMapa/images.tsx
+++ pruebaMapa/images.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="images" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+<tileset version="1.8" tiledversion="1.8.4" name="images" tilewidth="7" tileheight="7" tilecount="990" columns="45">
  <image source="images.jpg" width="318" height="158"/>
 </tileset>
