cb70ccbb57a98f42415f209cb8cf26c45488e577
cjcre <cjcrespo5@gmail.com>
2022-04-28 20:45:30 +0200

    mapa para nico

---
+++ pruebaMapa/images.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="images" tilewidth="7" tileheight="7" tilecount="990" columns="45">
+ <image source="images.jpg" width="318" height="158"/>
+</tileset>
