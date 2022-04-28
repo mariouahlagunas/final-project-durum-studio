d24774aaa2e3e3973ff3c88fdd7041c91d2afa5d
cjcre <cjcrespo5@gmail.com>
2022-04-28 20:55:35 +0200

    mapa para nico

--- pruebaMapa/soldado.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="soldado" tilewidth="7" tileheight="7" tilecount="110264" columns="308">
- <image source="soldado.png" width="2160" height="2506"/>
-</tileset>
