cb879574291d8cf506ddde75ffca5208e3787e24
cjcre <cjcrespo5@gmail.com>
2022-05-05 20:16:56 +0200

    prueba mapa nico y carlos

---
+++ assets/tilemaps/pruebaMapa3/enemigos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="enemigos" tilewidth="8" tileheight="8" tilecount="1" columns="1">
+ <image source="enemigos.png" width="8" height="8"/>
+</tileset>
---
+++ assets/tilemaps/pruebaMapa3/hierba1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="hierba1" tilewidth="10" tileheight="10" tilecount="300" columns="12">
+ <image source="hierba1.png" width="128" height="256"/>
+</tileset>
